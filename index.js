387453deecf6c8f9344f58de5a1ed296b367e161
Pal Pinter <pal@palpinter.com>
2017-03-27 15:22:03 +0200

    Rename template files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var program = require('commander');
 var createApplication = require('./lib/function/create-application.js')
+var createModule = require('./lib/function/create-module.js')
 
 program
   .command('create [appName]')
@@ -12,6 +13,15 @@ program
   .option('-a, --app', 'Create only meteor app without dirs and files')
   .action(function(appName, options){
     createApplication(appName, options);
+  });
+
+program
+  .command('module [moduleName]')
+  .alias('m')
+  .description('Create application module')
+  .option('-r, --reset', 'Remove existing module first')
+  .action(function(moduleName, options){
+    createModule(moduleName, options);
   });
 
 program.parse(process.argv);
