0909716482ef9ed528b0c10fec0ff0228cbe8ce1
Pal Pinter <pal@palpinter.com>
2017-03-27 10:11:14 +0200

    createApplication function

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var createApplication = require('./lib/function/create-application.js')
 
 program
   .command('create [appName]')
   .alias('c')
-  .description('run setup commands for all envs')
+  .description('Create meteor application')
   .action(function(appName, options){
-    console.log('create "%s" meteor application', appName);
+    createApplication(appName);
   });
 
 program.parse(process.argv);
