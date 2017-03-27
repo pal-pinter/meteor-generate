4d78c1d2f669346802a8ed65453178ad79c36765
Pál Pintér <pal@palpinter.com>
2017-03-27 09:12:47 +0200

    command interface redesign

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 #!/usr/bin/env node
 
 var program = require('commander');
-var shell = require('shelljs');
-var _ = require('lodash')
 
-const testFolder = '.';
-const fs = require('fs');
-list = fs.readdirSync(testFolder)
-
-console.log(list)
 program
-  .version('0.0.1')
-  .usage('<command> <argument>')
-  .option('-c, --create [name]', 'Create meteor application name of [name]', 'name')
+  .command('create [appName]')
+  .alias('c')
+  .description('run setup commands for all envs')
+  .action(function(appName, options){
+    console.log('create "%s" meteor application', appName);
+  });
 
 program.parse(process.argv);
-
-msg = _.upperFirst('create application')
-if (program.create) console.log(msg);
